1f3e93bb27defb77383dc4d2fd74fff81f4e7cc1
DESKTOP-I3USAS7\PC <champoyradoo@gmail.com>
2025-02-17 18:46:28 +0800

    feat: test widget

--- test/index.php
+++ test/index.php
@@ -127,9 +127,18 @@
 </style>
 <body>
 <div>
-    <script src="//widget.simplybook.me/v2/widget/widget.js" type="text/javascript"></script>
+    <div id="sbw_v81299"></div>
+
     <script type="text/javascript">
-      var widget = new SimplybookWidget({"widget_type":"iframe","url":"https:\/\/poychamp.simplybook.me","theme":"space","theme_settings":{"timeline_hide_unavailable":"1","sb_base_color":"#49306b","hide_past_days":"0","timeline_show_end_time":"0","timeline_modern_display":"as_slots","display_item_mode":"block","body_bg_color":"#ffffff","sb_review_image":"","dark_font_color":"#474747","light_font_color":"#ffffff","btn_color_1":"#c98e5a","sb_company_label_color":"#ffffff","hide_img_mode":"1","show_sidebar":"1","sb_busy":"#c7b3b3","sb_available":"#d6ebff"},"timeline":"classes_plugin","datepicker":"top_calendar","is_rtl":false,"app_config":{"clear_session":0,"allow_switch_to_ada":0,"predefined":[]}});
+      (function(w, d, s, i) {
+        var script = d.createElement(s);
+        script.async = true;
+        script.src = "//widget.simplybook.me/v2/widget/widget.js";
+        script.onload = function() {
+          new SimplybookWidget({"widget_type":"iframe","url":"https:\/\/poychamp.simplybook.me","theme":"space","theme_settings":{"timeline_hide_unavailable":"1","sb_base_color":"#49306b","hide_past_days":"0","timeline_show_end_time":"0","timeline_modern_display":"as_slots","display_item_mode":"block","body_bg_color":"#ffffff","sb_review_image":"","dark_font_color":"#474747","light_font_color":"#ffffff","btn_color_1":"#c98e5a","sb_company_label_color":"#ffffff","hide_img_mode":"1","show_sidebar":"1","sb_busy":"#c7b3b3","sb_available":"#d6ebff"},"timeline":"classes_plugin","datepicker":"top_calendar","is_rtl":false,"app_config":{"clear_session":0,"allow_switch_to_ada":0,"predefined":{"provider":"2","category":"1"}},"container_id":"sbw_v81299"});
+        };
+        d.head.appendChild(script);
+      })(window, document, 'script', 'sbw_v81299');
     </script>
 </div>
 </body>
