5c0a6a3d8c74ba09abc5b172a965134778939f8c
DESKTOP-I3USAS7\PC <champoyradoo@gmail.com>
2025-02-21 18:18:07 +0800

    feat: test widget

--- test/index.php
+++ test/index.php
@@ -134,10 +134,11 @@
     script.async = true;
     script.src = "//widget.simplybook.me/v2/widget/widget.js";
     script.onload = function() {
-      new SimplybookWidget({"widget_type":"iframe","url":"https:\/\/poychamp.simplybook.me","theme":"space","theme_settings":{"timeline_hide_unavailable":"1","sb_base_color":"#49306b","hide_past_days":"0","timeline_show_end_time":"0","timeline_modern_display":"as_slots","display_item_mode":"block","body_bg_color":"#ffffff","sb_review_image":"","dark_font_color":"#474747","light_font_color":"#ffffff","btn_color_1":"#c98e5a","sb_company_label_color":"#ffffff","hide_img_mode":"1","show_sidebar":"1","sb_busy":"#c7b3b3","sb_available":"#d6ebff"},"timeline":"classes_plugin","datepicker":"top_calendar","is_rtl":false,"app_config":{"clear_session":0,"allow_switch_to_ada":0,"predefined":{"provider":"2","category":"3"}},"container_id":"sbw_v81299"});
+      new SimplybookWidget({"widget_type":"iframe","url":"https:\/\/poychamp.simplybook.me","theme":"space","theme_settings":{"timeline_hide_unavailable":"1","sb_base_color":"#49306b","hide_past_days":"0","timeline_show_end_time":"0","timeline_modern_display":"as_slots","display_item_mode":"block","body_bg_color":"#ffffff","sb_review_image":"","dark_font_color":"#474747","light_font_color":"#ffffff","btn_color_1":"#c98e5a","sb_company_label_color":"#ffffff","hide_img_mode":"1","show_sidebar":"1","sb_busy":"#c7b3b3","sb_available":"#d6ebff"},"timeline":"modern","datepicker":"top_calendar","is_rtl":false,"app_config":{"clear_session":1,"allow_switch_to_ada":0,"predefined":{"provider":"2","service":"8","category":"3"}},"container_id":"sbw_v81299"});
     };
     d.head.appendChild(script);
   })(window, document, 'script', 'sbw_v81299');
 </script>
+
 </body>
 </html>
